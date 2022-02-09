8b20428990dc7dab0737d52ef7a10b44f77ca267
Seham-Shaldoum <97483348+Seham-Shaldoum@users.noreply.github.com>
2022-02-09 16:36:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
 let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
-'#A36C33', //middle strip 2
-'#042060', //middle bottom 3
-'#3778AE' //bottom 4
+'purple', //top 0
+'gray', //middle top 1
+'black', //middle strip 2
+'yellow', //middle bottom 3
+'green' //bottom 4
 ]
 
 
